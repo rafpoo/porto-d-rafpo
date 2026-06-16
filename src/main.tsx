8e2959efe13dd5a8c865c94e588bfed18707ab2d
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2026-06-16 16:51:00 +0700

    feat: added custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/pirata-one/400.css';
+import '@fontsource/rye/400.css';
+import '@fontsource/im-fell-english-sc/400.css';
+import '@fontsource/crimson-text/400.css';
+import '@fontsource/crimson-text/600.css';
+import '@fontsource/crimson-text/700.css';
+import '@fontsource/bangers/400.css';
 import App from './App';
 import './styles.css';
 
